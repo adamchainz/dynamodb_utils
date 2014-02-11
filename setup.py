10fe7e134073e7134c81b250e1703fcfffc77e28
Adam Chainz <adam@adamj.eu>
2014-02-11 17:08:23 +0000

    Rename to dynamodb_utils and scripts to dynamodb-

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name='ddb-utils',
-    version='0.1',
+    name='dynamodb_utils',
+    version='0.2',
     author='Adam Johnson',
     author_email='me@adamj.eu',
-    url='https://github.com/adamchainz/ddb-utils',
+    url='https://github.com/adamchainz/dynamodb_utils',
     description='A toolchain for AWS DynamoDB to make common operations easier.',
     long_description=open('README.rst').read(),
     license='GPLv3',
