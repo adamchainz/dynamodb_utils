a8bbd6cc78df86ac2d4d05485b99c9b6156c6f16
Adam Chainz <adam@adamj.eu>
2014-03-20 15:49:20 +0000

    Bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dynamodb_utils',
-    version='0.3',
+    version='0.4',
     author='Adam Johnson',
     author_email='me@adamj.eu',
     url='https://github.com/adamchainz/dynamodb_utils',
